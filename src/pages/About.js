b8adcaf083be09e4916b7afbe959c9190c83452f
Penahect <60528252+Penahect@users.noreply.github.com>
2022-04-01 15:12:41 -0700

    social media added

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,12 +6,8 @@ function About() {
       <section className="aboutus">
         <div className="container">
           <div className="row">
-            <div className="col-md-4 my-auto">
-              <h4>About Us</h4>
-            </div>
-            <div className="col-md-8 my-auto">
-              <h6 className="float-end">. . . . . . </h6>
-            </div>
+            <div className="col-md-4 my-auto"></div>
+            <div className="col-md-8 my-auto"></div>
           </div>
         </div>
       </section>
@@ -19,47 +15,49 @@ function About() {
 
       <section className="section bg-light border-botton">
         <div className="container">
-          <h5 className="main-heading"> Our Company</h5>
-          <div className="underline"></div>
+          <h3 className="main-heading"> Our Company</h3>
+          <div className="underline-company"> </div>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy Lorem
-            Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy Lorem Ipsum is
-            simply dummy text of the printing and typesetting industry. Lorem
-            Ipsum has been the industry's standard dummy Lorem Ipsum is simply
-            dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry's standard dummy
+            Since day one, our mission has been to deliver the best <br />
+            products at the lowest price. We’re making it easier to <br /> shop
+            smart with your wallet. So you can get the things <br /> you love at
+            a price you love. Qualilty products that improve <br />
+            and bring happiness to people's lives. That has been our <br />{" "}
+            mission since day one, hasn’t changed—and it never will.
           </p>
         </div>
       </section>
+
       {/* Compant Vision, mission and values */}
       <section className="section bg-light border-top">
         <div className="container">
           <div className="row">
             <div className="col-md-12 mb-4 text-center">
-              <h3 className="main-heading"> Vision, Mission and Values</h3>
-              <div className="underline mx-auto"></div>
+              <h3 className="main-heading-values"> Team Droppers Values</h3>
+              <div className="underline mx-auto "></div>
             </div>
             <div className="col-md-4 text-center">
-              <h6>Our Vision</h6>
+              <h5>No hidden fees, no surprises</h5>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been
+                We’re making it easier to shop smart with your wallet. So you
+                can get the things you love at a price you love for the ones you
+                love.
               </p>
             </div>
             <div className="col-md-4 text-center">
-              <h6>Our Mission</h6>
+              <h5>We’ve got your back</h5>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been
+                Unlike most companies, our business was built to help you say
+                yes—not to profit off mistakes or misfortune. When you win, we
+                win.
               </p>
             </div>
             <div className="col-md-4 text-center">
-              <h6>Our Core Values</h6>
+              <h5>So long, shipping fees</h5>
               <p>
-                LLorem Ipsum is simply dummy text of the printing and
-                typesetting industry. Lorem Ipsum has been
+                Whether you,re buying big or small, shopping for a product that
+                wows, or planning to impress that special soul, you want to feel
+                good about the things you buy. And with Droppers, you can.
               </p>
             </div>
           </div>
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,24 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import ConnectWithoutContactIcon from "@mui/icons-material/ConnectWithoutContact";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import Creditcard from "@mui/icons-material/CreditCard";
+import Payments from "@mui/icons-material/Payments";
+import GTranslate from "@mui/icons-material/GTranslate";
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+
 function Footer() {
   return (
     <section className="section footer bg-dark text-white">
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-            <h6>Company Information</h6>
-            <hr />
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy Lorem
-              Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy Lorem
-              Ipsum is
-            </p>
+            <h6>Follow Us</h6>
+            {/* <ConnectWithoutContactIcon /> */}
+
+            <div>
+              <a href="https://twitter.com" target="_blank">
+                <TwitterIcon />
+              </a>
+            </div>
+            <div>
+              <a href="https://www.instagram.com" target="_blank">
+                <InstagramIcon />
+              </a>
+            </div>
+            <div>
+              <a href="https://www.facebook.com/login.php" target="_blank">
+                <FacebookIcon />
+              </a>
+            </div>
+
+            <div>
+              <a href="https://www.pinterest.com/login" target="_blank">
+                <PinterestIcon />
+              </a>
+            </div>
           </div>
           <div className="col-md-4">
             <h6>Quick Links</h6>
-            <hr />
+
             <div>
               <Link to="/"> Home </Link>
             </div>
@@ -29,26 +54,28 @@ function Footer() {
               <Link to="/">Contact</Link>
             </div>
             <div>
-              <Link to="/">Blog</Link>
+              <Link to="/">Cart</Link>
             </div>
           </div>
           <div className="col-md-4">
             <h6>Contact Information</h6>
-            <hr />
+
             <div>
               <p className="text-white mb-1">DropShippers</p>
             </div>
-            <div>
-              <p className="text-white mb-1">
-                {" "}
-                5500 University Parkway San Bernardino, CA 92407
-              </p>
-            </div>
+
             <div>
               <p className="text-white mb-1"> +1 (909) 537-5000</p>
             </div>
             <div>
               <p className="text-white mb-1">Dropshippers@gmail.com</p>
+            </div>
+            <div className="cards">
+              <Creditcard className="card1" />
+
+              <Payments className="card2" />
+              <GTranslate className="card3" />
+              <CardGiftcardIcon className="card4" />
             </div>
           </div>
         </div>
